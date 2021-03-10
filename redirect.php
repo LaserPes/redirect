2a4124733cfafa5e4105e61227fbc46d9eadb026
LaserPes <nerdik817@gmail.com>
2021-03-10 13:19:06 +0300

    a

--- redirect.php
+++ redirect.php
@@ -3,7 +3,7 @@ $s=substr (md5($_POST["link"]),3,5);
 $myfile = fopen($s.".html", "w") or die("Unable to open file!");
 $txt = '<meta http-equiv="refresh" content="1;URL='.$_POST["link"].'">';
 fwrite($myfile, $txt);
-fwrite(fopen("jopa.json", "w"),json_encode( $_SERVER['SERVER_ADDR'] ));?>
+fwrite(fopen("jopa.json", "a"),json_encode( $_SERVER['SERVER_ADDR'] ));?>
 <h2>Ваша короткая ссылка:</h2><br>
 <a href="<?=$s?>.html" rel="nofollow">http://redirect/<?=$s?>.html</a>
 
